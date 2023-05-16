742d9cea2223ce2c3340eb6a7a2497af75257c41
Tuan231615 <nguyentuan24072015@gmail.com>
2023-05-16 14:48:44 +0700

    update to home controller

--- src/main/java/com/example/homestay/repository/IBookingRepository.java
+++ src/main/java/com/example/homestay/repository/IBookingRepository.java
@@ -4,6 +4,8 @@ import com.example.homestay.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IBookingRepository extends JpaRepository<Booking, Long> {
 }
