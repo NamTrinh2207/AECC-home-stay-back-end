8b40e4b227e9d49a7b91433c5e75a012eed2c425
NamTrinh2207 <trinhnam180697@gmail.com>
2023-05-24 10:20:11 +0700

    update query search

--- src/main/java/com/example/homestay/repository/HomeRepository.java
+++ src/main/java/com/example/homestay/repository/HomeRepository.java
@@ -8,7 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Date;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -17,25 +18,30 @@ public interface HomeRepository extends JpaRepository<Homes, Long> {
 
     Page<Homes> findByUsers_Id(Long userId, Pageable pageable);
 
-    @Query(value = "select distinct h.id, h.address, h.bedroom, h.bathroom, h.price_by_day, b.checkin, b.checkout " +
-            "from homes h " +
-            "         join booking b on h.id = b.home_id " +
-            "where (:address is null or h.address like concat('%', :address, '%')) " +
-            "    and (:bedroom is null or h.bedroom = :bedroom) " +
-            "    and (:bathroom is null or h.bathroom = :bathroom) " +
-            "    and (:min_price is null or :max_price is null) or (h.price_by_day between :min_price and :max_price) " +
-            "    and ( " +
-            "              (b.checkin is null or b.checkout is null) " +
-            "              or (b.checkin >= :start_date and b.checkin <= :end_date) " +
-            "              or (b.checkout >= :start_date and b.checkout <= :end_date) " +
-            "          ) " +
-            "group by h.id, b.id;", nativeQuery = true)
-    List<Homes> findByCriteria(@Param("bedroom") Integer bedroom,
+    @Query(value = "SELECT DISTINCT h.id,h.address,h.bedroom,h.bathroom,h.price_by_day, b.checkin,b.checkout,hi.image " +
+            "FROM homes h " +
+            "         JOIN booking b ON h.id = b.home_id " +
+            "         JOIN homes_image hi ON h.id = hi.homes_id " +
+            "WHERE :bedroom IS NULL OR h.bedroom = :bedroom " +
+            "   OR :bathroom IS NULL OR h.bathroom = :bathroom " +
+            "   OR :address IS NULL OR h.address LIKE CONCAT('%', :address, '%')" +
+            "   OR (" +
+            "        (:start_date IS NULL OR :end_date IS NULL) OR (b.checkin IS NULL OR b.checkout IS NULL)" +
+            "        OR (b.checkin >= :start_date AND b.checkin <= :end_date) " +
+            "        OR (b.checkout >= :start_date AND b.checkout <= :end_date) " +
+            "    ) " +
+            "   OR ( " +
+            "        (:min_price IS NULL OR :max_price IS NULL) " +
+            "        OR (h.price_by_day BETWEEN :min_price AND :max_price) " +
+            "    ) " +
+            "GROUP BY h.id, h.address, h.bedroom, h.bathroom, h.price_by_day, b.checkin, b.checkout, hi.image;",
+            nativeQuery = true)
+    List<Object[]> searchHomes(@Param("bedroom") Integer bedroom,
                                @Param("bathroom") Integer bathroom,
                                @Param("address") String address,
-                               @Param("start_date") Date startDate,
-                               @Param("end_date") Date endDate,
-                               @Param("min_price") Long minPrice,
-                               @Param("max_price") Long maxPrice);
+                               @Param("start_date") LocalDate startDate,
+                               @Param("end_date") LocalDate endDate,
+                               @Param("min_price") BigDecimal minPrice,
+                               @Param("max_price") BigDecimal maxPrice);
 
 }
