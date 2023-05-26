e30f5d07566abce3b6ab9782ab73647ff32f377f
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2023-05-26 09:14:20 +0700

    .

--- src/main/java/com/example/homestay/repository/IBookingRepository.java
+++ src/main/java/com/example/homestay/repository/IBookingRepository.java
@@ -3,6 +3,7 @@ package com.example.homestay.repository;
 import com.example.homestay.model.Booking;
 import com.example.homestay.model.DTO.IGetMostRentedBooking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
