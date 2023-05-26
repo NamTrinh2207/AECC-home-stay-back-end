625f95103fc4e01432327e8a5ef097f0ffc5a61c
luc-nguyen-174 <louiskingvn@gmail.com>
2023-05-26 08:33:45 +0700

    update review model, update cancel request model

--- src/main/java/com/example/homestay/model/Review.java
+++ src/main/java/com/example/homestay/model/Review.java
@@ -15,5 +15,46 @@ public class Review {
     @Lob
     private String comment;
 
+    public Review() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Homes getHomes() {
+        return homes;
+    }
+
+    public void setHomes(Homes homes) {
+        this.homes = homes;
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
