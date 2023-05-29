0544f2c8f6bfcece7f03faa7a455c3561ee8c478
huongly9596789 <huongly95967@gmail.com>
2023-05-29 13:51:39 +0700

    .

--- src/main/java/com/example/homestay/repository/IRentalHistoryRepository.java
+++ src/main/java/com/example/homestay/repository/IRentalHistoryRepository.java
@@ -12,8 +12,21 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IRentalHistoryRepository extends JpaRepository<RentalHistory, Long> {
 
-    @Query(nativeQuery = true,value = "SELECT b.checkin, b.checkout, b.total_price as totalPrice, h.address, h.bathroom, h.bedroom, h.description, h.name, h.price_by_day as priceByDay, hi.image\n" +
-            "            FROM rental_history r join booking b on b.id = r.booking_id join homes h on h.id = b.home_id join homes_image hi on h.id = hi.homes_id\n" +
-            "            group by b.checkin, b.checkout, b.total_price, h.address, h.bathroom, h.bedroom, h.description, h.name, h.price_by_day, hi.image")
+    @Query(nativeQuery = true,value = "SELECT b.id,\n" +
+            "    b.checkin,\n" +
+            "       b.checkout,\n" +
+            "       b.total_price  as totalPrice,\n" +
+            "       h.address,\n" +
+            "       h.bathroom,\n" +
+            "       h.bedroom,\n" +
+            "       h.description,\n" +
+            "       h.name,\n" +
+            "       h.price_by_day as priceByDay,\n" +
+            "       MAX(hi.image)  as image\n" +
+            "FROM rental_history r\n" +
+            "         join booking b on b.id = r.booking_id\n" +
+            "         join homes h on h.id = b.home_id\n" +
+            "         join homes_image hi on h.id = hi.homes_id\n" +
+            "group by b.id")
     Page<RentalHistoryHotel> listHistory(Pageable pageable);
 }
