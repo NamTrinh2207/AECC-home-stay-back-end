68d9668aa2a60ac243e049393ba0ee7963663aa5
luc-nguyen-174 <louiskingvn@gmail.com>
2023-06-02 13:11:20 +0700

    update review and rating class, controller

--- src/main/java/com/example/homestay/repository/IReviewRepository.java
+++ src/main/java/com/example/homestay/repository/IReviewRepository.java
@@ -1,9 +1,14 @@
 package com.example.homestay.repository;
 
+import com.example.homestay.model.Homes;
 import com.example.homestay.model.Review;
+import com.example.homestay.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IReviewRepository extends JpaRepository<Review, Long> {
+    Optional<Review> findByHomesAndUsers(Homes homes, Users users);
 }
--- src/main/java/com/example/homestay/service/review/IReviewService.java
+++ src/main/java/com/example/homestay/service/review/IReviewService.java
@@ -1,7 +1,12 @@
 package com.example.homestay.service.review;
 
+import com.example.homestay.model.Homes;
 import com.example.homestay.model.Review;
+import com.example.homestay.model.Users;
 import com.example.homestay.service.IGeneralService;
 
+import java.util.Optional;
+
 public interface IReviewService extends IGeneralService<Review> {
+    Optional<Review> findByHomesAndUsers(Homes homes, Users users);
 }
