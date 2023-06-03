b6eee84aeb3b82dc4831157167f5e10cdb27c21d
luc-nguyen-174 <louiskingvn@gmail.com>
2023-06-03 23:37:38 +0700

    update api show review by home id

--- src/main/java/com/example/homestay/service/review/IReviewService.java
+++ src/main/java/com/example/homestay/service/review/IReviewService.java
@@ -4,9 +4,12 @@ import com.example.homestay.model.Homes;
 import com.example.homestay.model.Review;
 import com.example.homestay.model.Users;
 import com.example.homestay.service.IGeneralService;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface IReviewService extends IGeneralService<Review> {
     Optional<Review> findByHomesAndUsers(Homes homes, Users users);
+
+    Iterable<Review> getReviewByHomeID(@Param("id") Long id);
 }
